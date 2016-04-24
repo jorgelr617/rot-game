8624d420b6561bfecba98c8654362dd896c6dece
Mike Gomez <mikegomezg@outlook.com>
2016-04-24 14:07:24 -0400

    Adjusted build settings to avoid errors.

--- Client/src/index.js
+++ Client/src/index.js
@@ -17,11 +17,12 @@ $(document).ready(function() {
   context.fillRect(0, 0, canvas.width, canvas.height);
   
   var startPoint = new Point(canvas.width/4, canvas.height/8);
+//comment
 	
 	var testMap = new Map(
     context,
 		canvas.width/22, // hex size,
-    10, // node radius
+    8, // node radius
 		5, // the minimum number of hexes in a column
 		7, // the maximum number of hexes in a column
     startPoint
