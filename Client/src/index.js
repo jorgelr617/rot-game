7d10240df81ace6b98ad5929ba8308a40a8d7e4e
Mike Gomez <mikegomezg@outlook.com>
2016-04-26 18:26:02 -0400

    Finalized development setup and started using D3.

--- Client/src/index.js
+++ Client/src/index.js
@@ -4,31 +4,37 @@ import {Point} from "./point.js"
 import {MapCreationAnimator} from "./mapCreationAnimator.js"
 import {Hex} from "./hex.js"
 import {Node} from "./node.js"
-
+import * as d3 from "d3";
 
 $(document).ready(function() {
   
-  var canvas = document.createElement("canvas");
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  document.body.appendChild(canvas);
-  var context = canvas.getContext("2d");
-	context.fillStyle = "white";
-  context.fillRect(0, 0, canvas.width, canvas.height);
-  
-  var startPoint = new Point(canvas.width/4, canvas.height/8);
-//comment
+  d3.select('.container').text('selected');
+  // board.width = window.innerWidth;
+  // board.height = window.innerHeight;
+
+	// d3.select("div")
+	// 	.append('svg')
+	// 	.attr('width', 600)
+	// 	.attr('height', 600);
+	//comment
 	
-	var testMap = new Map(
-    context,
-		canvas.width/22, // hex size,
-    8, // node radius
-		5, // the minimum number of hexes in a column
-		7, // the maximum number of hexes in a column
-    startPoint
-  );
-  
-  testMap.createColumn(startPoint);
+  // document.body.appendChild(board);
+  // var context = board.getContext("2d");
+	// context.fillStyle = "white";
+  // context.fillRect(0, 0, board.width, board.height);
+  //
+  // var startPoint = new Point(board.width/4, board.height/8);
+	//
+	// var testMap = new Map(
+  //   context,
+		// board.width/22, // hex size,
+  //   8, // node radius
+		// 5, // the minimum number of hexes in a column
+		// 7, // the maximum number of hexes in a column
+  //   startPoint
+  // );
+  //
+  // testMap.createColumn(startPoint);
     
 });
 
